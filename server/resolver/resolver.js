a04f04444c5a666b8823d39cce5c415eac8868a4
khanhdii-08 <phamduy2408@gmail.com>
2023-03-14 16:37:41 +0700

    part 2 connect mongodb

--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -1,27 +1,32 @@
-const { books, authors } = require("../data/static");
-
 const resolvers = {
   //Query
   Query: {
-    books: () => books,
-    book: (parent, args) => books.find((book) => book.id == args.id),
-    authors: () => authors,
-    author: (parent, args) => authors.find((author) => author.id == args.id),
+    books: async (parent, args, { mongoDataMethods }) =>
+      await mongoDataMethods.getAllBooks(),
+    book: async (parent, args, { mongoDataMethods }) =>
+      await mongoDataMethods.getBook(args.id),
+    authors: async (parent, args, { mongoDataMethods }) =>
+      await mongoDataMethods.getAllAuthors(),
+    author: async (parent, args, { mongoDataMethods }) =>
+      await mongoDataMethods.getAuthor(args.id),
   },
 
   Book: {
-    author: (parent, args) =>
-      authors.find((author) => author.id == parent.authorId),
+    author: async (parent, args, { mongoDataMethods }) =>
+      await mongoDataMethods.getAuthor(parent.authorId),
   },
 
   Author: {
-    books: (parent, args) => books.filter((book) => book.authorId == parent.id),
+    books: (parent, args, { mongoDataMethods }) =>
+      mongoDataMethods.getAllBooks({ authorId: parent.id }),
   },
 
   // Mutation
   Mutation: {
-    craeteAuthor: (parent, args) => args,
-    createBook: (parent, args) => args,
+    createAuthor: async (parent, args, { mongoDataMethods }) =>
+      await mongoDataMethods.createAuthor(args),
+    createBook: async (parent, args, { mongoDataMethods }) =>
+      mongoDataMethods.createBook(args),
   },
 };
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
+require("dotenv").config();
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
 
 // load schema & reslover
 const typeDefs = require("./schema/schema");
 const resolvers = require("./resolver/resolver");
 
+//load db methods
+const mongoDataMethods = require("./data/db");
+
+//Connect to MongoDb
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: () => ({ mongoDataMethods }),
 });
 
 const app = express();
